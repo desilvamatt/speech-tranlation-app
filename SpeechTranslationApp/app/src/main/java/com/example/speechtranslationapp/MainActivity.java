4e19007ab67a0dd35b87e48b2adf6d2800f9a639
desilvamatt <m7n9k@unb.ca>
2019-02-21 22:37:45 -0400

    text to speech and speech to text now functional :squirrel:

--- SpeechTranslationApp/app/src/main/java/com/example/speechtranslationapp/MainActivity.java
+++ SpeechTranslationApp/app/src/main/java/com/example/speechtranslationapp/MainActivity.java
@@ -1,50 +1,71 @@
 package com.example.speechtranslationapp;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import android.content.ActivityNotFoundException;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.Locale;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
-    private static final int REQ_CODE_SPEECH_INPUT = 100;
-    private TextView mVoiceInputTv;
-    private ImageButton mSpeakBtn;
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private TextToSpeech tts;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
-        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
-        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+        txtSpeechInput = (TextView) findViewById(R.id.voiceInput);
+        tts = new TextToSpeech(this, this);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startVoiceInput();
+                promptSpeechInput();
             }
         });
     }
 
-    private void startVoiceInput() {
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please begin speaking");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
-
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
+    /**
+     * Receiving speech input
+     * */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -52,12 +73,43 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    mVoiceInputTv.setText(result.get(0));
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                    speak();
                 }
                 break;
             }
 
         }
     }
-}
+
+
+    private void speak(){
+        tts.speak(txtSpeechInput.getText().toString(),TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "This language is not supported", Toast.LENGTH_LONG).show();
+            }
+            else{
+                promptSpeechInput();
+            }
+        }else{
+            Toast.makeText(this, "Initialization failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }}
