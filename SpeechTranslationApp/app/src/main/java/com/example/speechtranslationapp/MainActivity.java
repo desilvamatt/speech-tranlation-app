7acc34133aa3b21f4a36b24c03a9c584389aa89f
desilvamatt <m7n9k@unb.ca>
2019-03-24 23:27:18 -0300

    translate to french functional :squirrel:

--- SpeechTranslationApp/app/src/main/java/com/example/speechtranslationapp/MainActivity.java
+++ SpeechTranslationApp/app/src/main/java/com/example/speechtranslationapp/MainActivity.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import android.content.ActivityNotFoundException;
-import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
@@ -25,6 +33,15 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private ImageButton btnSpeak;
     private TextToSpeech tts;
     private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    private final String requestURL = "https://www.googleapis.com/language/translate/v2?key=";
+    //TODO Add your own key
+    private final String requestKey = "\n";
+    private final String requestSrc = "&source=";
+    private final String requestDst = "&target=";
+    private final String requestTxt = "&q=";
+    private final String formatTxt = "&format=text";
+
 
 
     @Override
@@ -41,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 promptSpeechInput();
             }
         });
+
     }
 
 
@@ -76,19 +94,72 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtSpeechInput.setText(result.get(0));
-                    speak();
-                }
-                break;
+                    getTranslation(result.get(0),"en", "fr");
+                    }
             }
-
+            break;
         }
+
     }
 
 
-    private void speak(){
-        tts.speak(txtSpeechInput.getText().toString(),TextToSpeech.QUEUE_FLUSH, null, null);
+
+    private void speak(String text){
+        Locale aLocale = new Locale("fr","FR");
+        tts.setLanguage(aLocale);
+        tts.speak(text,TextToSpeech.QUEUE_FLUSH, null, null);
     }
+
+    //TODO
+    public void getTranslation( String txt, String src, String dst ){
+        String request = requestURL + requestKey + requestSrc + src + requestDst + dst+ formatTxt + requestTxt + txt;
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, request, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+
+                        JSONObject test = null;
+                        try {
+                            test = response.getJSONObject("data");
+                            txtSpeechInput.setText(test.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        JSONArray translations = null;
+                        try {
+                            translations = test.getJSONArray("translations");
+                            txtSpeechInput.setText(translations.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            JSONObject translated_text = translations.getJSONObject(0);
+                            String final_translation = translated_text.getString("translatedText");
+                            txtSpeechInput.setText(final_translation);
+                            String translated = txtSpeechInput.getText().toString().trim();
+                            speak(translated);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", "nope");
+                    }
+                }
+        );
+        queue.add(getRequest);
+    }
+
+
 
     @Override
     public void onInit(int status) {
